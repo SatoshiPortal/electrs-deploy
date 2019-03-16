94847666a43935a27f3619e6c2504fa3bf302d97
kexkey <github@kexkey.com>
2019-03-15 20:02:06 -0400

    Added SSL with LE-companion and traefik and scripts

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ cd electrs
 docker build -t electrs-app .
 cd ..
 mkdir -p ~/data/tor/hidden_service
-cp tor/torrc ~/data/tor/
-chmod 700 ~/data/tor/hidden_service
+sudo cp tor/torrc ~/data/tor/
+sudo chmod 700 ~/data/tor/hidden_service
 sudo chown -R 2000:2000 ~/data/tor
